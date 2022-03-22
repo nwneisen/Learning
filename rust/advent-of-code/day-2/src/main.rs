3d385a29913ac9cf523fe948a5e6cf846e3d4afa
Nick Neisen <nwneisen@gmail.com>
2022-03-21 22:05:27 -0600

    Day 2

--- rust/advent-of-code/day-2/src/main.rs
+++ rust/advent-of-code/day-2/src/main.rs
@@ -1,3 +1,27 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let input = fs::read_to_string("input").expect("Error reading input from file");
+    let lines: Vec<_> = input.lines().collect();
+
+    let mut position = 0;
+    let mut depth = 0;
+    let mut aim = 0;
+
+    for line in lines {
+        let direction = line.split(" ").nth(0).unwrap();
+        let distance = line.split(" ").nth(1).unwrap();
+
+        let magnitude = distance.parse::<i32>().unwrap();
+        if direction == "forward" {
+            position += magnitude;
+            depth += magnitude * aim;
+        } else if direction == "up" {
+            aim -= magnitude;
+        } else if direction == "down" {
+            aim += magnitude;
+        }
+    }
+
+    println!("{}", position * depth);
 }
