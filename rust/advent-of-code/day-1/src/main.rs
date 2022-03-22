5f4744ed072ca1866c0f5bdd971ad5fdcdc7f07a
Nick Neisen <nwneisen@gmail.com>
2022-03-21 21:37:13 -0600

    Second part of day 1

--- rust/advent-of-code/day-1/src/main.rs
+++ rust/advent-of-code/day-1/src/main.rs
@@ -4,17 +4,24 @@ fn main() {
     let input = fs::read_to_string("./input").expect("something went wrong reading the file");
     let lines: Vec<_> = input.lines().collect();
 
-    let mut prev = lines[0].parse::<i32>().unwrap();
+    let mut prev = 0;
     let mut count = 0;
+    let mut sum = 0;
+
     for (idx, line) in lines.iter().enumerate() {
-        if idx == 0 {
+        if idx <= 2 {
+            sum += line.parse::<i32>().unwrap();
+            prev = sum;
             continue;
         }
-        let curr = line.parse::<i32>().unwrap();
-        if curr - prev > 0 {
+
+        sum += line.parse::<i32>().unwrap();
+        sum -= lines[idx - 3].parse::<i32>().unwrap();
+
+        if sum - prev > 0 {
             count += 1;
         }
-        prev = curr;
+        prev = sum;
     }
 
     println!("{}", count);
